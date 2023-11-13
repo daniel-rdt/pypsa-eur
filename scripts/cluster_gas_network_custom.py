6b64c4b56af1979c5fdbbe5c2f084724f65d4ce7
daniel.rdt <daniel.ruedt@campus.tu-berlin.de>
2023-11-13 18:28:28 +0100

    add custom gas network and add new h2 pipeline typology with connections to all onshore neighbor regions. also fix h2 and gas network with extendable set to False

---
+++ scripts/cluster_gas_network_custom.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: : 2020-2023 The PyPSA-Eur Authors
+#
+# SPDX-License-Identifier: MIT
+"""
+Cluster gas transmission network to clustered model regions.
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+from packaging.version import Version, parse
+from pypsa.geo import haversine_pts, haversine
+from shapely import wkt
+from shapely.geometry import LineString
+
+
+def concat_gdf(gdf_list, crs="EPSG:4326"):
+    """
+    Concatenate multiple geopandas dataframes with common coordinate reference
+    system (crs).
+    """
+    return gpd.GeoDataFrame(pd.concat(gdf_list), crs=crs)
+
+
+def load_bus_regions(onshore_path, offshore_path):
+    """
+    Load pypsa-eur on- and offshore regions and concat.
+    """
+    bus_regions_offshore = gpd.read_file(offshore_path)
+    bus_regions_onshore = gpd.read_file(onshore_path)
+    bus_regions = concat_gdf([bus_regions_offshore, bus_regions_onshore])
+    bus_regions = bus_regions.dissolve(by="name", aggfunc="sum")
+
+    return bus_regions
+
+
+def load_custom_gas_network(path):
+    """
+    Loads and cleans custom gas network dataset.
+    """
+    df = pd.read_csv(path, decimal=",", sep=";", index_col=0)
+    # drop columns that are not needed
+    df.drop(columns=["node0", "node1", "node0_x", "node0_y", "node1_x", "node1_y"], inplace=True)
+    # turn coordinates into points and drop redundant columns
+    df["point0"] = gpd.points_from_xy(df.node0_long, df.node0_lat)
+    df["point1"] = gpd.points_from_xy(df.node1_long, df.node1_lat)
+    df.drop(columns=["node0_long", "node0_lat", "node1_long", "node1_lat"], inplace=True)
+    # convert from GWh/day to MWh/h
+    df["cap_per_day_p80_GWh"] = df.cap_per_day_p80_GWh * 1e3 / 24
+    # rename columns to pypsa names
+    df.rename(columns={"cap_per_day_p80_GWh": "p_nom", "diameter": "diameter_mm"}, inplace=True)
+    # set all pipes in custom dataset as bidirectional
+    df["bidirectional"] = 1
+
+    return df
+
+
+def build_clustered_gas_network(df, bus_regions, length_factor=1.25):
+    for i in [0, 1]:
+        gdf = gpd.GeoDataFrame(geometry=df[f"point{i}"], crs="EPSG:4326")
+
+        kws = (
+            dict(op="within")
+            if parse(gpd.__version__) < Version("0.10")
+            else dict(predicate="within")
+        )
+        bus_mapping = gpd.sjoin(gdf, bus_regions, how="left", **kws).index_right
+        bus_mapping = bus_mapping.groupby(bus_mapping.index).first()
+
+        df[f"bus{i}"] = bus_mapping
+
+        df[f"point{i}"] = df[f"bus{i}"].map(
+            bus_regions.to_crs(3035).centroid.to_crs(4326)
+        )
+
+    # drop pipes where not both buses are inside regions
+    df = df.loc[~df.bus0.isna() & ~df.bus1.isna()]
+
+    # drop pipes within the same region
+    df = df.loc[df.bus1 != df.bus0]
+
+    # recalculate lengths as center to center * length factor
+    df["length"] = df.apply(
+        lambda p: length_factor
+                  * haversine_pts([p.point0.x, p.point0.y], [p.point1.x, p.point1.y]),
+        axis=1,
+    )
+
+    # tidy and create new numbered index
+    # df["line"] = df.apply(lambda row: LineString([row['point0'], row['point1']]), axis=1) #Create a linestring column
+    df.drop(["point0", "point1"], axis=1, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+    reindex_pipes(df)
+
+    strategies = {
+        "bus0": "first",
+        "bus1": "first",
+        "p_nom": "sum",
+        "diameter_mm": "mean",
+        "length": "mean",
+        "name": " ".join,
+        "p_min_pu": "min",
+    }
+    df = aggregate_parallel_pipes(df, strategies)
+
+    return df
+
+
+def reindex_pipes(df):
+    def make_index(x):
+        connector = " <-> " if x.bidirectional else " -> "
+        return "gas pipeline " + x.bus0 + connector + x.bus1
+
+    df.index = df.apply(make_index, axis=1)
+
+    df["p_min_pu"] = df.bidirectional.apply(lambda bi: -1 if bi else 0)
+    df.drop("bidirectional", axis=1, inplace=True)
+
+    df.sort_index(axis=1, inplace=True)
+
+
+def aggregate_parallel_pipes(df, strategies):
+    """
+    Aggregate pipelines and dataset according to given strategies
+    """
+    return df.groupby(df.index).agg(strategies)
+
+
+def add_geometries(df, bus_regions, map=False):
+    """
+    returns a gpd.GeoDataFrame with a linestring as geometry
+    """
+    # add points corresponding to centroid of bus regions
+    if map:
+        for i in [0, 1]:
+            df[f"point{i}"] = df[f"bus{i}"].map(
+                bus_regions.to_crs(3035).centroid.to_crs(4326)
+            )
+    # Create a linestring column
+    df["line"] = df.apply(lambda row: LineString([row['point0'], row['point1']]), axis=1)
+
+    return gpd.GeoDataFrame(data=df, geometry=df.line, crs="EPSG:4326")
+
+
+def filter_for_country(df, ctry):
+    """
+    Filters a DataFrame with pypsa bus nodes for a specific country
+    """
+    return df.loc[(df.bus0.str.startswith(ctry)) & (df.bus1.str.startswith(ctry))]
+
+
+def substitute_country_network(df1, df2, ctry):
+    """
+    Substitutes gas pipelines for given country in pypsa network
+    """
+    return pd.concat(
+        [
+            (df1[~(df1.bus0.str.startswith(ctry) & df1.bus1.str.startswith(ctry))]),
+            df2
+        ], axis=0).sort_index()
+def find_neighbors(gdf):
+    """
+    Finds neighbors for regions in GeoDataFrame and stores in separate column
+    """
+    for index, row in gdf.iterrows():
+        neighbors = gdf[gdf.geometry.touches(row['geometry'])].index.to_list()
+        gdf.at[index, "neighbors"] = neighbors
+        # add again as list if only one entry
+        if len(neighbors) == 1:
+            gdf.at[index, "neighbors"] = list(neighbors)
+    return gdf[["neighbors"]]
+
+
+if __name__ == "__main__":
+    if "snakemake" not in globals():
+        from _helpers import mock_snakemake
+
+        snakemake = mock_snakemake("cluster_gas_network_custom", simpl="", clusters="180")
+
+    logging.basicConfig(level=snakemake.config["logging"]["level"])
+
+    fn_custom = snakemake.input.gas_network_custom
+    fn = snakemake.input.cleaned_gas_network
+    df = pd.read_csv(fn, index_col=0)
+    for col in ["point0", "point1"]:
+        df[col] = df[col].apply(wkt.loads)
+
+    df_custom = load_custom_gas_network(fn_custom)
+
+    bus_regions = load_bus_regions(
+        snakemake.input.regions_onshore, snakemake.input.regions_offshore
+    )
+    onshore_regions = gpd.read_file(snakemake.input.regions_onshore).set_index("name")
+    offshore_regions = gpd.read_file(snakemake.input.regions_offshore).set_index("name")
+
+    # clustered gas network from old dataset
+    gas_network = build_clustered_gas_network(df, bus_regions)
+
+    # clustered gas network from custom addition
+    gas_network_custom = build_clustered_gas_network(df_custom, bus_regions)
+    gas_network_custom_de = filter_for_country(gas_network_custom, "DE")
+
+    # merge gas networks to one
+    gas_network_merged = substitute_country_network(gas_network, gas_network_custom_de, "DE")
+
+    gas_network_merged.to_csv(snakemake.output.clustered_gas_network_custom)
+
+    # prepare bus onshore regions neighbors for h2 network preparation
+    regions_neighbors = find_neighbors(onshore_regions)
+    regions_neighbors.to_parquet(snakemake.output.bus_regions_neighbors)
+
+    # calculate centroid distances in km between all bus regions
+    bus_regions_centroids = gpd.GeoDataFrame(geometry=bus_regions.to_crs(3035).centroid.to_crs(4326))
+    distancematrix = haversine(np.dstack((bus_regions_centroids.geometry.x, bus_regions_centroids.geometry.y))[0],
+              np.dstack((bus_regions_centroids.geometry.x, bus_regions_centroids.geometry.y))[0])
+    distance_df = pd.DataFrame(data=distancematrix,
+                               index=bus_regions_centroids.index.values,
+                               columns=bus_regions_centroids.index.values)
+    distance_df.to_csv(snakemake.output.bus_regions_centroid_distances)
--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -327,6 +327,21 @@ def create_network_topology(
         [n.lines[ln_attrs], n.links.loc[n.links.carrier.isin(carriers), lk_attrs]]
     ).fillna(0)
 
+    # for h2 network add connections to onshore regional neighbors to topology
+    # distances are center to center distances between the regions
+    if options.get("H2_network", True):
+        regions_neighbors = pd.read_parquet(snakemake.input.bus_regions_neighbors)
+        regions_distances = pd.read_csv(snakemake.input.bus_regions_centroid_distances, index_col=0)
+        for index, row in regions_neighbors.iterrows():
+            for neighbor in row.neighbors:
+                candidate = pd.DataFrame({
+                    "bus0": index,
+                    "bus1": neighbor,
+                    "length": regions_distances.at[index, neighbor],
+                    "underwater_fraction": 0.0}, index=[0])
+                candidates = pd.concat([candidates, candidate], ignore_index=True)
+
+
     # base network topology purely on location not carrier
     candidates["bus0"] = candidates.bus0.map(n.buses.location)
     candidates["bus1"] = candidates.bus1.map(n.buses.location)
@@ -338,6 +353,8 @@ def create_network_topology(
     candidates = pd.concat([candidates_p, candidates_n])
 
     def make_index(c):
+        if bidirectional:
+            connector = "<->"
         return prefix + c.bus0 + connector + c.bus1
 
     topo = candidates.groupby(["bus0", "bus1"], as_index=False).mean()
@@ -1152,7 +1169,11 @@ def add_storage_and_grids(n, costs):
     )
 
     if options["gas_network"] or options["H2_retrofit"]:
-        fn = snakemake.input.clustered_gas_network
+        if snakemake.params.gas_network_custom:
+            # option to add alternate gas network infrastructure
+            fn = snakemake.input.clustered_gas_network_custom
+        else:
+            fn = snakemake.input.clustered_gas_network
         gas_pipes = pd.read_csv(fn, index_col=0)
 
     if options["gas_network"]:
@@ -1172,6 +1193,11 @@ def add_storage_and_grids(n, costs):
                 gas_pipes.length * costs.at["CH4 (g) pipeline", "fixed"]
             )
 
+        if snakemake.params.fix_H2:
+            p_nom_extendable = False
+        else:
+            p_nom_extendable = True
+
         n.madd(
             "Link",
             gas_pipes.index,
@@ -1179,7 +1205,7 @@ def add_storage_and_grids(n, costs):
             bus1=gas_pipes.bus1 + " gas",
             p_min_pu=gas_pipes.p_min_pu,
             p_nom=gas_pipes.p_nom,
-            p_nom_extendable=True,
+            p_nom_extendable=p_nom_extendable,
             p_nom_max=gas_pipes.p_nom_max,
             p_nom_min=gas_pipes.p_nom_min,
             length=gas_pipes.length,
@@ -1256,6 +1282,11 @@ def add_storage_and_grids(n, costs):
         to = "H2 pipeline retrofitted"
         h2_pipes = gas_pipes.rename(index=lambda x: x.replace(fr, to))
 
+        if snakemake.params.fix_H2:
+            p_nom_extendable = False
+        else:
+            p_nom_extendable = True
+
         n.madd(
             "Link",
             h2_pipes.index,
@@ -1263,7 +1294,7 @@ def add_storage_and_grids(n, costs):
             bus1=h2_pipes.bus1 + " H2",
             p_min_pu=-1.0,  # allow that all H2 retrofit pipelines can be used in both directions
             p_nom_max=h2_pipes.p_nom * options["H2_retrofit_capacity_per_CH4"],
-            p_nom_extendable=True,
+            p_nom_extendable=p_nom_extendable,
             length=h2_pipes.length,
             capital_cost=costs.at["H2 (g) pipeline repurposed", "fixed"]
             * h2_pipes.length,
@@ -1279,6 +1310,11 @@ def add_storage_and_grids(n, costs):
             n, "H2 pipeline ", carriers=["DC", "gas pipeline"]
         )
 
+        if snakemake.params.fix_H2:
+            p_nom_extendable = False
+        else:
+            p_nom_extendable = True
+
         # TODO Add efficiency losses
         n.madd(
             "Link",
@@ -1286,7 +1322,7 @@ def add_storage_and_grids(n, costs):
             bus0=h2_pipes.bus0.values + " H2",
             bus1=h2_pipes.bus1.values + " H2",
             p_min_pu=-1,
-            p_nom_extendable=True,
+            p_nom_extendable=p_nom_extendable,
             length=h2_pipes.length.values,
             capital_cost=costs.at["H2 (g) pipeline", "fixed"] * h2_pipes.length.values,
             carrier="H2 pipeline",
@@ -3264,8 +3300,8 @@ if __name__ == "__main__":
             opts="",
             clusters="180",
             ll="v1.5",
-            sector_opts="8760H-T-H-B-I-A-solar+p3-cb30ex0",
-            planning_horizons="2030",
+            sector_opts="200H-T-H-B-I-A-solar+p3",
+            planning_horizons="2045",
         )
 
     logging.basicConfig(level=snakemake.config["logging"]["level"])
